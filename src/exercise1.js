3c0ad4f4136c46d513cb7ebf328c457326a8f509
nirv44 <nicolas44.vivion@gmail.com>
2018-03-02 09:58:26 +0100

    exercice 1

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,7 +1,35 @@
 const moment = require("moment");
 
 const sortProduct = input => {
-  // Your future job begins here ...
+
+	var r = input
+	.filter(val => moment(val.dateAdded).year() >= 2000)
+	.reduce((acc,val) => {
+
+		if(acc[val.name]){
+			acc[val.name].push({
+				[val.name] : {
+					'name': val.name,
+					'year': moment(val.dateAdded).year(),
+					'mont': moment(val.dateAdded).month(),
+					'quantity': val.quantity
+				}
+			});
+		}else{
+			acc.push({
+				[val.name] : {
+					'name': val.name,
+					'year': moment(val.dateAdded).year(),
+					'mont': moment(val.dateAdded).month(),
+					'quantity': val.quantity
+				}
+			});
+		}
+
+		return acc;
+	}, [{}])
+	
+	return r;
 };
 
 module.exports = {
