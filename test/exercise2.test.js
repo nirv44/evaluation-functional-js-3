7ad6281bf40a64e46a766eca511ade915eefd1e9
nirv44 <nicolas44.vivion@gmail.com>
2018-03-02 10:11:34 +0100

    exercice 2

--- test/exercise2.test.js
+++ test/exercise2.test.js
@@ -1,11 +1,46 @@
 const expect = require('chai').expect;
 const program = require('../src/exercise2');
 
-describe(`${program.title}`, function () {
+describe.only(`${program.title}`, function () {
 
   it('Function yolo with mysterious parameter should return a list of messages', function () {
 
-    const whereIsMyMind = "???";
+   
+
+    const whereIsMyMind = [
+      {
+        'sex': 1,
+        'name' : "William"
+      },
+      {
+        'sex': 1,
+        'name' : "Gaius"
+      },
+      {
+        'sex': 1,
+        'name' : "Lee"
+      },
+      {
+        'sex': 2,
+        'name' : "Kara"
+      },
+      {
+        'sex': 2,
+        'name' : "Laura"
+      },
+      {
+        'sex': 9,
+        'name' : "Number six"
+      },
+      {
+        'sex': 9,
+        'name' : "Number height"
+      }
+    ];
+
+
+
+
 
     const output = [
       "0 not known character(s) ",
